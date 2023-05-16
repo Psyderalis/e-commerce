59cfa3f542e6aff78dd7aaff03448e7f6b816e1e
Melanie Tolvett S <melanietolvett@gmail.com>
2023-05-16 13:32:01 -0400

    agregando archivos

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+// SE UNIFICAN LOS COMPONENTES Y PAGES 
+// Router
+
+function App() {
+
+  return (
+    <div>
+      <button type="button" className="btn btn-dark">FUNCIONA BS</button>
+    </div>
+  )
+}
+
+export default App
